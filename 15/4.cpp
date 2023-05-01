3ec59e6bf2dc850c29c8a756b5ef6365dc19f90b
Григорий Попов <115580534+gr1hon@users.noreply.github.com>
2023-05-01 16:55:33 +0300

    Update 4.cpp

--- 15/4.cpp
+++ 15/4.cpp
@@ -4,30 +4,37 @@
 using namespace std;
 
 int main(){
-    vector<int> vec = {-100, -20, -10, 0};
-    vector<int> positive;
-    vector<int> negative;
-
-    for (int i = 0; i < vec.size(); ++i) {
-        if (vec[i] < 0){
-            negative.push_back(vec[i]);
-        } else positive.push_back(vec[i]);
-    }
-
-    int countP = 0;
-    int countN = negative.size()-1;
-
-    for (int i = 0; i < vec.size(); ++i) {
-        if(positive[countP] > -negative[countN]){
-            vec[i] = negative[countN];
-            countN--;
-        } else{
-            vec[i] = positive[countP];
-            countP++;
+    vector<int> vec = {-100, 1, 3, 6, 10};
+    vector<int> answer(vec.size());
+    int countP = -1;
+    int countN = -1;
+    for (int i = 1; i < vec.size(); ++i) {
+        if (vec[i] >= 0 && vec[i-1] < 0){
+            countN = i - 1;
+            countP = i;
+            break;
         }
     }
-
+    if (countP == -1 && vec[0] < 0){
+        for (int i = 0; i < vec.size(); ++i) {
+            answer[i] = vec[vec.size()-1-i];
+        }
+    } else if (countN == -1){
+        for (int i = 0; i < vec.size(); ++i) {
+            answer[i] = vec[i];
+        }
+    } else{
+        for (int i = 0; i < vec.size(); ++i) {
+            if(countP == vec.size() || vec[countP] > -vec[countN] && countN >= 0){
+                answer[i] = vec[countN];
+                countN--;
+            } else {
+                answer[i] = vec[countP];
+                countP++;
+            }
+        }
+    }
     for (int i = 0; i < vec.size(); ++i) {
-        cout << vec[i] << " ";
+        cout << answer[i] << " ";
     }
 }
