2baaef8059e1ebad9c4424ab50d67677b6cb1706
Григорий Попов <115580534+gr1hon@users.noreply.github.com>
2023-05-01 12:52:25 +0300

    Create 3.cpp

---
+++ 15/3.cpp
@@ -0,0 +1,62 @@
+////Быстрая сортировка
+//
+#include <iostream>
+//#include <vector>
+//
+using namespace std;
+//
+//int partition(vector<int> numbers, int l, int r){
+//    int m = numbers[(l + r) / 2];
+//    int i = l;
+//    int j = r;
+//    while(i <= j){
+//        while(numbers[i] < m){
+//            i++;
+//        }
+//        while(numbers[j] > m){
+//            j--;
+//        }
+//        if(i >= j){
+//            break;
+//        }
+//        int tmp;
+//        tmp = numbers[i];
+//        numbers[i++] = numbers[j];
+//        numbers[j--] = tmp;
+//    }
+//    return j;
+//}
+//
+//void quicksort(vector<int> numbers, int l, int r){
+//    if(l < r){
+//        int q = partition(numbers, l, r);
+//        quicksort(numbers, l, q);
+//        quicksort(numbers, q + 1, r);
+//    }
+//}
+
+int main(){
+    cout << "123";
+}
+
+
+
+//int main(){
+//    vector<int> vec;
+//    int number = 0;
+//    while(number != -2){
+//        cin >> number;
+//        if (number != -1){
+//            vec.push_back(number);
+//        } else if (vec.size() < 5){
+//            cout << "Count of vec less than 5!\n";
+//        } else{
+//            quicksort(vec, 0, vec.size()-1);
+//            for (int i = 0; i < vec.size(); ++i) {
+//                cout << vec[i] << " ";
+//            }
+//            cout << "\n" << vec[4] << "\n";
+//        }
+//    }
+//    cout << "End";
+//}
