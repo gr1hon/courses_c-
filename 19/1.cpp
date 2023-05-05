85e91d7f7f576a033827dc820371c7e26abb7870
Григорий Попов <115580534+gr1hon@users.noreply.github.com>
2023-05-05 17:33:33 +0300

    Create 1.cpp

---
+++ 19/1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream file;
+    file.open("C:\\documents\\untitled\\words.txt");
+    string word;
+    cout << "input word:\n";
+    cin >> word;
+    int count = 0;
+    while(!file.eof()){
+        string s;
+        file >> s;
+        if (s==word)
+            count++;
+    }
+    file.close();
+    cout << count;
+}
