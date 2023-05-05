1d682e4fba9ab56889e5cb56be51052fe0e94312
Григорий Попов <115580534+gr1hon@users.noreply.github.com>
2023-05-05 20:36:32 +0300

    Create 3.cpp

---
+++ 19/3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream file;
+    file.open("C:\\documents\\untitled\\table.txt");
+    int maxMoney = 0;
+    string maxMoneyOwner;
+    int sum = 0;
+    if (file.is_open()){
+        while(!file.eof()){
+            string name;
+            string surname;
+            int x;
+            string date;
+            file >> name >> surname >> x >> date;
+            if (x > maxMoney){
+                maxMoney = x;
+                maxMoneyOwner = name;
+                maxMoneyOwner += " ";
+                maxMoneyOwner += surname;
+            }
+            sum += x;
+        }
+        cout << "Total number of issues " << sum << endl;
+        cout << "The largest payout belongs to " << maxMoneyOwner << endl;
+    } else{
+        cout << "Error open file!";
+    }
+    file.close();
+}
