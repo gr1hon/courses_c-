7ae7427a34327db63128933098b604787ec672b7
Григорий Попов <115580534+gr1hon@users.noreply.github.com>
2023-05-12 14:35:54 +0300

    Create 1.cpp

---
+++ 24/1.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <ctime>
+#include <vector>
+
+using namespace std;
+
+struct task{
+    string name = "unknown";
+    time_t start_time{};
+    time_t end_time{};
+    bool ended = false;
+};
+
+int main() {
+    vector<task> tasks;
+    while(true){
+        string command;
+        cout << "Enter the command(begin, end, status or exit):" << endl;
+        cin >> command;
+        task curr_task;
+        if (command == "begin"){
+            if (!tasks.empty() && !tasks[tasks.size()-1].ended){
+                tasks[tasks.size()-1].ended = true;
+                tasks[tasks.size()-1].end_time = ::time(nullptr);
+                cout << "Task " << tasks[tasks.size()-1].name << " finished!" << endl;
+            }
+            task new_task;
+            cout << "Enter the name of the task:" << endl;
+            cin >> new_task.name;
+            new_task.start_time = ::time(nullptr);
+            tasks.push_back(new_task);
+            cout << "Task " << tasks[tasks.size()-1].name << " started!" << endl;
+
+        } else if (command == "end"){
+            if (!tasks.empty() && !tasks[tasks.size()-1].ended){
+                tasks[tasks.size()-1].ended = true;
+                tasks[tasks.size()-1].end_time = ::time(nullptr);
+                cout << "Task " << tasks[tasks.size()-1].name << " finished!" << endl;
+            }
+
+
+        } else if (command == "status"){
+            for (const task& task : tasks) {
+                if (task.ended){
+                    double dif = ::difftime(task.end_time, task.start_time);
+                    cout << task.name << ", time spent " << dif << " seconds" <<endl;
+                } else
+                    cout << "Current task: " << task.name << endl;
+            }
+        } else if (command == "exit"){
+            return 0;
+        } else
+            cout << "Unknown command!" << endl;
+    }
+}
