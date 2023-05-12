7ad67f03c37ce67704f5a03f5747e4da74216b08
Григорий Попов <115580534+gr1hon@users.noreply.github.com>
2023-05-12 20:40:37 +0300

    Create 3.cpp

---
+++ 24/3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <chrono>
+
+using namespace std;
+
+
+int main() {
+    time_t t = time(nullptr);
+    tm start_time = *localtime(&t);
+    cout << "Enter the start_time you want to record (MM:SS):" << endl;
+    cin >> get_time(&start_time, "%M:%S");
+    int seconds = start_time.tm_sec + start_time.tm_min * 60;
+    
+//    auto start = std::chrono::steady_clock::now();
+//    int k = 0;
+//    while(k < seconds){
+//        auto curr_time = std::chrono::steady_clock::now();
+//        if (curr_time - start >= std::chrono::seconds(1)){
+//            start = curr_time;
+//            k++;
+//            cout << k/60 << ":" << k%60 << endl;
+//        }
+//    }
+//    cout << "DING! DING! DING!" << endl;
+//    
+//    Как я понял, выводить ответ в консоль раз в секунду одной командой (как в питоне time.sleep(1) вроде бы) нельзя.
+//    Поэтому придумал еще одно решение без доп библиотек, но оно какое то некрасивое((
+
+    t = time(nullptr);
+    time_t curr_time = t;
+    int k; int prev = -1;
+    while(curr_time - t < seconds){
+        curr_time = time(nullptr);
+        k = curr_time - t;
+        if (prev != k){
+            cout << k/60 << ":" << k%60 << endl;
+            prev = k;
+        }
+    }
+    cout << "DING! DING! DING!" << endl;
+}
