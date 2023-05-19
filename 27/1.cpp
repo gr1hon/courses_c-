24416b538ffb3387f1f92dab69c2ef3f7329341e
Григорий Попов <115580534+gr1hon@users.noreply.github.com>
2023-05-19 21:01:44 +0300

    Update 1.cpp

--- 27/1.cpp
+++ 27/1.cpp
@@ -7,9 +7,7 @@ class Branch{
 
 private:
     string elfName {};
-    Branch* parent = nullptr;
     vector<Branch*> children;
-    int childNumber = 0;
 
 public:
     Branch* getChildren(int i){
@@ -20,35 +18,34 @@ public:
         return elfName;
     }
 
-    void setElfName(string elfName) {
-        this->elfName = elfName;
+    void setElfName(string in_elfName) {
+        this->elfName = in_elfName;
     }
 
     void addChild(){
         auto* branch = new Branch();
-        branch->parent = this;
-        branch->childNumber = children.size();
         children.push_back(branch);
     }
 
-    bool isThereElf(string name){
-        if (!this->elfName.empty() && this->elfName == name){
-            return true;
-        } else{
-            for (int i = 0; i < this->children.size(); ++i) {
-                cout << "* ";
-                this->children[i]->isThereElf(name);
+    bool isThereElf(const string& in_elfName){
+        for (int i = 0; i < this->children.size(); ++i) {
+            if (this->children[i]->getElfName() == in_elfName)
+                return true;
+            for (int j = 0; j < this->children[i]->children.size(); ++j) {
+                if (this->children[i]->children[j]->getElfName() == in_elfName)
+                    return true;
             }
         }
+        return false;
     }
 
-    int bigBranchNumber(string in_elfName){
-        if (this->elfName == in_elfName){
-            if (parent->parent == nullptr) return childNumber;
-            return parent->childNumber;
-        } else{
-            for (int i = 0; i < children.size(); ++i) {
-                children[i]->bigBranchNumber(in_elfName);
+    int bigBranchNumber(const string& in_elfName){
+        for (int i = 0; i < this->children.size(); ++i) {
+            if (this->children[i]->getElfName() == in_elfName)
+                return i;
+            for (int j = 0; j < this->children[i]->children.size(); ++j) {
+                if (this->children[i]->children[j]->getElfName() == in_elfName)
+                    return i;
             }
         }
     }
@@ -76,10 +73,10 @@ int max(vector<int> vec){
     return ans;
 }
 
-void initialization(vector<Branch>& branches){
-    for (int i = 0; i < 1; ++i) { // 5
+void initialization(vector<Branch*>& branches){
+    for (int i = 0; i < 2; ++i) { // 5, тестить долго
         auto* branch = new Branch();
-        int b1 = 1; //  rand() %3 + 3
+        int b1 = rand() %3 + 3;
         for (int j = 0; j < b1; ++j) {
             branch->addChild();
             cout << "Enter the name of the elf on " << i+1 << " tree, " <<  j + 1 << " big branch:" << endl;
@@ -88,36 +85,35 @@ void initialization(vector<Branch>& branches){
             if (name != "None"){
                 branch->getChildren(j)->setElfName(name);
             }
-            int b2 = 3; // rand() % 2 + 2
+            int b2 = rand() % 2 + 2;
             for (int k = 0; k < b2; ++k) {
                 branch->getChildren(j)->addChild();
                 cout << "Enter the name of the elf on " << i+1 << " tree, " << j + 1 << " big branch, "
-                    << k + 1 << " small branch:" << endl;
+                     << k + 1 << " small branch:" << endl;
                 cin >> name;
                 if (name != "None"){
                     branch->getChildren(j)->getChildren(k)->setElfName(name);
                 }
             }
         }
-        branches.push_back(*branch);
+        branches.push_back(branch);
     }
 }
 
 int main() {
     srand(time(nullptr));
-    vector<Branch> trees;
+    vector<Branch*> trees;
     initialization(trees);
     string name;
     cout << "Enter the name of the elf you want to find:" << endl;
     cin >> name;
     vector<int> neighbors;
     for (int i = 0; i < trees.size(); ++i) {
-        cout << trees[i].isThereElf(name) << endl;
-//        if (trees[i].isThereElf(name))
-//            neighbors.push_back(trees[i].getChildren(trees[i].bigBranchNumber(name))->neighbors());
-//        else
-//            cout << "The elf with the name " << name << " was not found" << endl;
+        if (trees[i]->isThereElf(name)){
+            cout << "Elf " << name << " has " <<trees[i]->getChildren(trees[i]->bigBranchNumber(name))->neighbors() <<  " neighbors" << endl;
+            return 0;
+        }
+        delete trees[i];
     }
-//    int ans = max(neighbors);
-//    cout << "Elf " << name << " has " << ans << " neighbors" << endl;
+    cout << "The elf with the name " << name << " was not found" << endl;
 }
