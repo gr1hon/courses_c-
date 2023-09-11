e1ddcafb38c1f509a0e1e1cb93ceb80bf5b64645
Григорий Попов <115580534+gr1hon@users.noreply.github.com>
2023-09-11 18:32:27 +0300

    Add files via upload

--- 38/2/main.cpp
+++ 38/2/main.cpp
@@ -1,10 +1,32 @@
 #include <QApplication>
 #include <QPushButton>
+#include <QWebEngineView>
+#include <QHBoxLayout>
+#include <QMainWindow>
+#include <QPlainTextEdit>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
-    return QApplication::exec();
+    auto* window = new QWidget;
+    auto* hbox = new QHBoxLayout(window);
+
+    auto* text = new QPlainTextEdit;
+    QSizePolicy spLeft(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    spLeft.setHorizontalStretch(1);
+    text->setSizePolicy(spLeft);
+    hbox->addWidget(text);
+
+    auto* webView = new QWebEngineView;
+    QSizePolicy spRight(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    spRight.setHorizontalStretch(1);
+    webView->setSizePolicy(spRight);
+    hbox->addWidget(webView);
+    QObject::connect(text, &QPlainTextEdit::textChanged, [webView, text](){
+        auto html = text->toPlainText();
+        webView->setHtml(html);
+    });
+    window->show();
+    window->resize(600, 600);
+    return a.exec();
 }
